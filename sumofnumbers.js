7c4542ac3527e6ea9fe4cf69793746b6d6a98a4b
beejayines <beejayi@hawaii.edu>
2017-09-16 23:52:22 -1000

    Finished sumofnumbers

---
+++ sumofnumbers.js
@@ -0,0 +1,36 @@
+function sumFor(list) {
+  let total = 0;
+  for (let i = 0; i < list.length; i++) {
+    total += list[i];
+  }
+  return total;
+}
+
+function sumWhile(list) {
+  let total = 0;
+  let i = 0;
+  while (i < list.length) {
+    total += list[i];
+    i++;
+  }
+  return total;
+}
+
+function sumRecursion(list) {
+  if (list.length === 0) {
+    return 0;
+  }
+  return list[0] + sumRecursion(list.slice(1, list.length));
+}
+
+function sumTheSimpleWay(list) {
+  return _.reduce(list, function (memo, num) { return memo + num; }, 0);
+}
+
+const list = [1, 2, 3, 4, 5];
+
+console.log(sumFor(list));
+console.log(sumWhile(list));
+console.log(sumRecursion(list));
+console.log(sumTheSimpleWay(list));
+
